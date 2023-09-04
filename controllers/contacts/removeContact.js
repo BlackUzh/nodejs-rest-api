e54990b75b3ee05bbcdbc2ddf98ace6a1fedc36e
Oleksandr Shmaida <black7panthera@gmail.com>
2023-09-04 16:59:41 +0300

    task4

--- controllers/contacts/removeContact.js
+++ controllers/contacts/removeContact.js
@@ -1,12 +1,12 @@
 const { Contact } = require("../../models/contact");
 
-const {HttpError} = require("../../helpers");
+const { HttpError } = require("../../helpers");
 
 const removeContact = async (req, res) => {
   const { contactId } = req.params;
   const result = await Contact.findByIdAndRemove(contactId);
   if (!result) {
-    throw HttpError(404, "Not found");
+    throw HttpError(404);
   }
   res.json({ message: "contact deleted" });
 };
