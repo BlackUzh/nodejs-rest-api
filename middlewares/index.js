bfea71c120ee695a9b2500253b97bb3fa34166bc
Oleksandr Shmaida <black7panthera@gmail.com>
2023-09-10 17:37:56 +0300

    hw5

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,6 +1,8 @@
 const isValidId = require("./isValidId");
 const validateBody = require("./validateBody");
 const authenticate = require("./authentificate");
+const upload = require("./upload");
+
 
 module.exports = {
   isValidId,
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,11 +2,15 @@ const express = require("express");
 
 const ctrl = require("../../controllers/users");
 const { schemas } = require("../../models/user");
-const { validateBody, authenticate } = require("../../middlewares");
+const { validateBody, authenticate, upload } = require("../../middlewares");
 
 const router = express.Router();
 
-router.post("/register", validateBody(schemas.registerSchema), ctrl.registerUser);
+router.post(
+  "/register",
+  validateBody(schemas.registerSchema),
+  ctrl.registerUser
+);
 
 router.post("/login", validateBody(schemas.loginSchema), ctrl.loginUser);
 
@@ -14,6 +18,18 @@ router.post("/logout", authenticate, ctrl.logoutUser);
 
 router.get("/current", authenticate, ctrl.getCurrentUser);
 
-router.patch("/", authenticate, validateBody(schemas.updateStatusSchema), ctrl.updateStatusUser);
+router.patch(
+  "/",
+  authenticate,
+  validateBody(schemas.updateStatusSchema),
+  ctrl.updateStatusUser
+);
+
+router.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatar"),
+  ctrl.updateAvatar
+);
 
 module.exports = router;
